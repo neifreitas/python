a085b18dd199271cd46514787c86d493557c37b5
Nei Freitas <122657509+neifreitas@users.noreply.github.com>
2024-01-10 18:17:50 -0300

    Delete 03 - Dicionarios directory

--- 03 - Dicionarios/user.py
+++
@@ -1,9 +0,0 @@
-user_0 = {
-    'username': 'efermi',
-    'first': 'enrico',
-    'last': 'fermi'
-}
-
-for chave, valor in user_0.items():
-    print(f"\nChave: {chave}")
-    print(f"Valor: {valor}")
